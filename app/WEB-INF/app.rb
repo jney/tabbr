20318182591950b2998f168a2615eb2bfe291c53
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-04-28 20:46:32 +0200

    updated

--- app/WEB-INF/app.rb
+++ app/WEB-INF/app.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 
 set :environment, :development
 
+helpers do
+  def clickable_menu
+    @_out_buf << '<div class="menu"><span class="arrow"></span><div class="menu-content">'
+    yield
+    @_out_buf << '</div></div>'
+  end
+end
+
 get '/' do
   erb :index
 end
