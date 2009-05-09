9089aea91ede972658168c8cefd6b63aaaa685f9
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-05-09 13:10:43 +0200

    full up

--- app/WEB-INF/app.rb
+++ app/WEB-INF/app.rb
@@ -16,30 +16,28 @@ helpers do
 end
 
 get "/" do
-  if(session["base"].nil?)
-    redirect "/home"
+  if(session[:base].nil? || session[:base] == {})
+    redirect "/login"
   else
-    redirect "/timeline"
+    @tweets = session[:base].friends_timeline
+    erb :timeline
   end
 end
 
-get "/home" do
-  erb :home	
+get "/login" do
+  erb :login
 end
 
 post "/login" do
-  httpauth = Twitter::HTTPAuth.new(params["login"],params["pwd"])
-  session["base"] = Twitter::Base.new(httpauth)	
-  redirect "/timeline"
-end
-
-get "/timeline" do
-  timeline = []
-  request.params["friends_timeline"] = session["base"].friends_timeline
-  erb :timeline
+  if httpauth = Twitter::HTTPAuth.new(params["login"],params["pwd"])
+    session[:base] = Twitter::Base.new(httpauth)
+    redirect "/"
+  else
+    redirect "/login"
+  end
 end
 
 post "/tweet" do
-  session["base"].update(params["tweet"])
-  redirect "/timeline"
+  session[:base].update(params["tweet"])
+  redirect "/"
 end
--- app/WEB-INF/public/javascripts/application.js
+++ app/WEB-INF/public/javascripts/application.js
@@ -1,5 +1,9 @@
 $(function(){
   $("#tabs").tabs().find(".ui-tabs-nav").sortable({axis:'x'});
   
-  $(".menu").click(function(){$(this).children(".menu-content").toggle().toggleClass("ui-corner-bottom");})
+  $(".menu").click(function(){$(this).children(".menu-content").toggle().toggleClass("ui-corner-bottom");});
+  
+  function majText(text){
+    $("#counter").html = 140 - text.value.length;
+  }
 });
